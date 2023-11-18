fe671dfe7b3d72a74c0e93d92809a6bfa8dd74f4
Nianqing Yao <imbearchild@outlook.com>
2023-11-18 15:24:00 +0800

    feat: more manpage

--- misc/inert_doc.py
+++ misc/inert_doc.py
@@ -9,43 +9,34 @@ file = open("./src/tarpitd.py","r+")
 # 读取文件内容
 content = file.read()
 
-# 找到两个 "MARK_HERE" 标记的位置
-start = content.index(DOC_WRAP)
-start = content.index("tarpitd.py.1",start)
-start = content.index(DOC_START,start) + len(DOC_START)
-end = content.index(DOC_WRAP,start)
+manpages = ["tarpitd.py.1","tarpitd.py.7"]
+start = 0
+end = 0
 
-print(f"{start},{end}")
+for manual_name in manpages:
+    # 找到两个标记的位置
+    start = content.index(DOC_WRAP)
+    start = content.index(manual_name,start)
+    start = content.index(DOC_START,start) + len(DOC_START)
+    end = content.index(DOC_WRAP,start)
 
-# 如果找到了两个标记，就替换它们之间的内容
-if start != -1 and end != -1 and start < end:
+    print(f"{start},{end}")
 
-    manual_name = "tarpitd.py.1"
-    # 生成一个新的内容，可以根据需要修改
-    new_content = open(f"./misc/{manual_name}.md","r").read()
-    lines = new_content.splitlines()
-    new_content = ""
-    # 遍历每一行
-    for line in lines:
-        # 如果这一行以 “\” 结尾，就把它替换为 “\\”
-        if line.endswith("\\"):
-            line = line.replace("\\", "\\\\")
-        new_content += line +  "\n"
-        # if len(line) < 70 :
-        #     line = line + " " * (74 - len(line)) 
-        # new_content += '\"' + line + ' \\n\"' + "\n"
+    # 如果找到了两个标记，就替换它们之间的内容
+    if start != -1 and end != -1 and start < end:
 
-    # 用新的内容替换原来的内容
-    content = content[:start] + f"\n_MANUAL_{manual_name.upper().replace(".","_")}=\"\"\"\n" + new_content + "\n"+ "\"\"\"" +"\n" +content[end:]
-    print(content)
-    file.close()
+        # 生成一个新的内容，可以根据需要修改
+        new_content = open(f"./misc/{manual_name}.md","r").read()
 
-    with open("./src/tarpitd.py","w") as f:
+        # 用新的内容替换原来的内容
+        content = content[:start] + f"\n_MANUAL_{manual_name.upper().replace(".","_")}=r\"\"\"\n" + new_content + "\n"+ "\"\"\"" +"\n" +content[end:]
+    else:
+        # 打印错误信息
+        print("Could not find two MARK_HERE tags in the file")
+
+with open("./src/tarpitd.py","w") as f:
     # 写入新的内容
-        f.write(content)
+    f.write(content)
     
     # 打印成功信息
-    print("File updated successfully")
-else:
-    # 打印错误信息
-    print("Could not find two MARK_HERE tags in the file")
+print("File updated successfully")
--- src/tarpitd.py
+++ src/tarpitd.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # Manual: tarpitd.py.1
 # -----------------------------------------------------------------------------
-_MANUAL_TARPITD_PY_1="""
+_MANUAL_TARPITD_PY_1=r"""
 ## NAME
 
 tarpitd.py - a daemon making a port into tarpit
@@ -15,7 +15,9 @@ tarpitd.py - a daemon making a port into tarpit
 
 Tarpitd.py will listen on specified ports and trouble clients that 
 connect to it. For more information on tarpitd.py, please refer to 
-[tarpitd-service(7)](./tarpitd-serivce.7.md)
+[tarpitd.py(7)](./tarpitd.py.7.md), or use:
+
+    tarpitd.py --manual tarpitd.py.7
 
 ## EXAMPLES
 
@@ -30,12 +32,119 @@ seconds:
 
 Start two different HTTP tarpit at the same time:
 
-    tarpitd.py -s http_deflate_bomb:0.0.0.0:8080 \\
+    tarpitd.py -s http_deflate_bomb:0.0.0.0:8080 \
                   HTTP_ENDLESS_COOKIE:0.0.0.0:8088 
 
 ## AUTHOR
 
 Nianqing Yao [imbearchild at outlook.com]
+
+"""
+# =============================================================================
+
+# =============================================================================
+# Manual: tarpitd.py.7
+# -----------------------------------------------------------------------------
+_MANUAL_TARPITD_PY_7=r"""
+## NAME
+
+tarpitd.py - information about tarpit services in tarpitd.py
+
+## GENERAL DESCRIPTION
+
+Note: This section is for general information. And for description
+on services, please refer to SERVICES section.
+
+### TL;DR
+
+Tarpitd.py will listen on specified ports and trouble clients that 
+connect to it.
+
+### What is a "tarpit"
+
+According to Wikipedia: A tarpit is a service on 
+a computer system (usually aserver) that purposely delays 
+incoming connections. The concept is analogous with a tar pit, in
+which animals can get bogged down and slowly sink under the surface,
+like in a swamp. 
+
+Tarpitd.py will partly simulate common internet services,
+for an instance, HTTP, but respond in a way that may make the 
+client not work properly, slow them down or make them crash.
+
+### Why I need a "tarpit"
+
+This is actually a good thing in some situations.
+For example, an evil ssh client may connect to port 22,and tries to 
+log with weak passwords. Or evil web crawlers can collect information
+from your server, providing help for cracking your server.
+
+You can use tarpit to slow them down.
+
+### What is a service in tarpitd.py 
+
+A service in tarpitd.py represent a pattern of response.
+
+For an instance, to fight a malicious HTTP client, tarpitd.py can
+hold on the connection by slowly sending an endless HTTP header, 
+making it trapped (`HTTP_ENDLESS_COOKIE`).
+Also, tarpitd.py can send a malicious HTML back
+to the malicious client, overloading its HTML parser 
+(`HTTP_DEFLATE_HTML_BOMB`). 
+
+Different responses have different consequences, and different 
+clients may handle the same response 
+differently. So even for one protocol, there may be more than 
+one `service` in tarpitd.py correspond to it.
+
+## SERVICES
+
+### HTTP_ENDLESS_COOKIE
+
+Tested with client: Firefox, Chromium, curl
+
+Making the client hang by sending an endless HTTP header lines of
+`Set-Cookie:`. Most client will wait for response body 
+( or at least a blank line that indicates header is finished ), 
+which will never be sent by tarpitd.py.
+
+### HTTP_DEFLATE_HTML_BOMB
+
+Tested with client: Firefox, Chromium
+
+A badly formed HTML compressed by deflate (zlib) will be sent by 
+tarpitd.py. It's so bad that most client will waste a lot of time
+(more precisely, CPU time ) on parsing it.
+
+Some client won't bother to prase HTML, so this may not useful
+for them.
+
+### HTTP_DEFLATE_SIZE_BOMB
+
+Tested with client: Firefox, Chromium, curl
+
+Feeding client with a lot of compressed zero. The current 
+implementation sends a compressed 1 MB file, which is approximately 
+1 GB decompressed, plus some invalid HTML code to trick client.
+
+Curl won't decompress content by default. If you want to test this 
+with curl, please add `--compressed` option to it, and make sure you
+have enough space for decompressed data.
+
+### MISC_EGSH_AMINOAS
+
+Tested with client: openssh
+
+This service can be used as an alternative to endlessh.
+
+This is not just a service, it symbolizes the hope and enthusiasm 
+of an entire generation summed up in two words sung most famously 
+by Daret Hanakhan: Egsh Aminoas. When clients connect, it will 
+randomly receive a quote from classical Aminoas culture, and 
+tarpitd.py will show you the same quote in log at the same time.
+
+Hope this song will bring you homesickness. And remenber ... 
+You are always welcome in Aminoas.
 
 """
 # =============================================================================
@@ -301,11 +410,14 @@ async def async_main(args):
 
 def display_manual_unix(name):
     import subprocess
+    match name:
+        case "tarpitd.py.1":
+            subprocess.run("less", input=_MANUAL_TARPITD_PY_1.encode())
+        case "tarpitd.py.7":
+            subprocess.run("less", input=_MANUAL_TARPITD_PY_7.encode())
 
-    subprocess.run("less", input=_MANUAL_TARPITD_PY_1.encode())
 
-
-def main():
+def main_cli():
     logging.basicConfig(level=logging.INFO)
     import argparse
 
@@ -334,12 +446,12 @@ def main():
     )
 
     parser.add_argument(
-        "--manual", help="show full manual of this program", action="store_true"
+        "--manual", help="show full manual of this program",nargs="?", const="tarpitd.py.1", action="store"
     )
     args = parser.parse_args()
 
     if args.manual:
-        display_manual_unix("")
+        display_manual_unix(args.manual)
         pass
     elif args.serve:
         asyncio.run(async_main(args))
@@ -348,4 +460,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main_cli()
